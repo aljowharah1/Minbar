4fc54e02d07ac7bafadf177793a92a2902d67d14
3! <aljowharah74@gmail.com>
2025-06-27 18:57:40 +0200

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,31 @@
+// import the sequelize library
+const Sequelize = require('sequelize');
+
+// create a configuration object
+const sequelize = new Sequelize(
+  process.env.DB_NAME,   //database name
+  process.env.DB_USER,     // database user
+  process.env.DB_PWD,
+  {
+    // specify the database server used
+    dialect: 'mysql',
+
+    // We are using a cloud-based mysql instance on aiven.io so host is 'mysql-se371-psu-7eaf.k.aivencloud.com'
+    host: process.env.DB_HOST,
+
+    // Communication port on the database server
+    port: process.env.DB_PORT
+  }
+);
+
+const connectToDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log(`Successfully connected to database server...`);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Export the sequelize object.  
+module.exports = { sequelize, connectToDB };
